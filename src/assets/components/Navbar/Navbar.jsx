0551a3e6e10df63a0c80e82d2d7f322fb42e87b9
Raya Irfansyah <xrayirf@gmail.com>
2023-02-10 11:19:08 +0700

    add navbar

--- src/assets/components/Navbar/Navbar.jsx
+++ src/assets/components/Navbar/Navbar.jsx
@@ -8,28 +8,28 @@ const Navbar = () => {
     ];
     let [open, setOpen]=useState(false);
     return (
-        // <div className="shadow-wd w-full fixed top-0 left-0">
-        //     <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
-        //         <div className="font-bold text-2xl cursor-pointer flex items-center font[Poppins] text-gray-800">
-        //             Raya Irfansyah
-        //         </div>
+        <div className="shadow-wd w-full fixed top-0 left-0">
+            <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
+                <div className="font-bold text-2xl cursor-pointer flex items-center font[Poppins] text-gray-800">
+                    Raya Irfansyah
+                </div>
 
-        //         <div onClick={()=>setOpen(!open)} className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden">
-        //         <ion-icon name={open ? 'close':'menu'}></ion-icon>
-        //         </div>
+                <div onClick={()=>setOpen(!open)} className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden">
+                <ion-icon name={open ? 'close':'menu'}></ion-icon>
+                </div>
 
-        //         <ul className={`md:flex md:items-center md:pb-0 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'opacity-100':'top-[-490px]'} md:opacity-100 opacity-0 `}>
-        //             {
-        //                 Links.map((link)=>(
-        //                     <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
-        //                         <a href={link.link} className="text-gray-800 hover:text-gray-400 duration-500">{link.name}</a>
-        //                     </li>
-        //                 ))
-        //             }
-        //         </ul>
-        //     </div>
-        // </div>
-        <div className="bg-transparent absolute top-0 left-0 w-full flex items-center z-10"></div>
+                <ul className={`md:flex md:items-center md:pb-0 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'opacity-100':'top-[-490px]'} md:opacity-100 opacity-0 `}>
+                    {
+                        Links.map((link)=>(
+                            <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
+                                <a href={link.link} className="text-gray-800 hover:text-gray-400 duration-500">{link.name}</a>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
+        </div>
+        // <div className="bg-transparent absolute top-0 left-0 w-full flex items-center z-10"></div>
         )
 }
 
